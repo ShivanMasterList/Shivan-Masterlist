d7766d69fba943b1b9663efc2618903ee45a0c40
ShivanMasterList <167604069+ShivanMasterList@users.noreply.github.com>
2024-04-20 20:15:34 +1200

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1LPhEc4BWLI0Hbzx8PUz3BrH3hEIarvHgJ-iQyIS-n0M";
 
 
 /* ------------------------------------------------------------------- */
